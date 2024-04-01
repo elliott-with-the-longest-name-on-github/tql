e966776521349341e3449249b9bb236742e4a506
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2024-04-01 17:19:08 -0600

    Revert "feat: Snowflake dialect"
    This reverts commit e5cef37609c9e243cc46cd0f4d4b1f3afd0d6c72.

--- packages/tql-template/src/dialects/snowflake.ts
+++
@@ -1,6 +0,0 @@
-import type { DialectImpl } from '../types.js';
-import { PostgresLike } from '../dialect.js';
-
-export class SnowflakeDialect extends PostgresLike implements DialectImpl {
-	protected parameterPrefix = ':';
-}
--- packages/tql-template/src/index.ts
+++ packages/tql-template/src/index.ts
@@ -18,7 +18,6 @@ import type { Tql } from './types.js';
 export type * from './nodes.ts';
 export type * from './types.js';
 export { PostgresDialect } from './dialects/postgres.js';
-export { SnowflakeDialect } from './dialects/snowflake.js';
 
 export const init: Init = ({ dialect }) => {
 	const fragment = Object.defineProperty(
