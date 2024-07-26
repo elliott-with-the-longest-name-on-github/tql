036f83f2fcd5d7a4d6caef5dc555d8f36cf7ab83
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-07-26 09:46:50 +0100

    feat: export MySQLDialect from the index file

--- packages/tql-template/src/index.ts
+++ packages/tql-template/src/index.ts
@@ -18,6 +18,7 @@ import type { Tql } from './types.js';
 export type * from './nodes.ts';
 export type * from './types.js';
 export { PostgresDialect } from './dialects/postgres.js';
+export { MySQLDialect } from './dialects/mysql.js';
 
 export const init: Init = ({ dialect }) => {
 	const fragment = Object.defineProperty(
